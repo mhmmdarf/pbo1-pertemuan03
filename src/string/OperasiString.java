0b7a4364afd6ce31420a6a142a6c5ddfc6048df0
mhmmdarf <mhmmdarf4@gmail.com>
2020-05-03 00:51:21 +0800

    toUppercase untuk merubah suatu char maupun String menjadi huruf besar toLowercase untuk merubah suatu char maupun String menjadi huruf kecil substring (5) untuk memotong string, atau untuk mengambil sebagian nilai dari sebuah string (5 = Karta) substring (0,4) untuk memotong string, atau untuk mengambil sebagian nilai dari sebuah string (0,4 = Yogy)

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -5,12 +5,17 @@ import java.sql.SQLOutput;
 public class OperasiString {
     public static void main(String[] args) {
 
-        String kota = "Martapura";
+        String kota = "Yogyakarta";
 
         System.out.println(kota);
 
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
