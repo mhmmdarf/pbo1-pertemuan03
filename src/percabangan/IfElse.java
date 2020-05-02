d958284fb4e07cc1ba62dfaa5691a1ad93f22494
mhmmdarf <mhmmdarf4@gmail.com>
2020-05-03 03:23:03 +0800

    If Else Statement 2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,8 +2,8 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 1000000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 50000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang anda kurang, silahkan kembali");
