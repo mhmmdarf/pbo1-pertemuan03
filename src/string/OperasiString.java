489224486719678cba155500d7d167ea781c6c9e
mhmmdarf <mhmmdarf4@gmail.com>
2020-05-03 00:23:46 +0800

    Membuat String dari array Char

---
+++ src/string/OperasiString.java
@@ -0,0 +1,16 @@
+package string;
+
+import java.sql.SQLOutput;
+
+public class OperasiString {
+    public static void main(String[] args) {
+
+        String kota = "Martapura";
+
+        System.out.println(kota);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+    }
+}
